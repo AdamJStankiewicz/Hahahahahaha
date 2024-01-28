a41d4f4f63d47d0fc675ac322c11c8d1c73a0779
Robbie Cooper <cooperra@users.noreply.github.com>
2024-01-27 21:07:22 -0500

    Add madlib minigame (see README for how to add more madlibs)

---
+++ Minigames/madlibs/README.txt
@@ -0,0 +1,6 @@
+To add a madlib template in two steps:
+
+- 1. Create a new timeline in the Dialogic panel. Look to madlib1.dtl for inspiration.
+- 2. Edit madlib_controller.gd to add your timeline name to the list.
+
+That's literally everything. Your timeline will be picked up by the system and added to the rotation.
---
+++ Minigames/madlibs/madlib_autoload.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var available_madlibs = [
+	"madlib1",
+	"madlib2",
+]
+
+@onready var remaining_madlibs = available_madlibs.duplicate()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	remaining_madlibs.shuffle()
+
+func has_next_madlib() -> bool:
+	return not remaining_madlibs.is_empty()
+
+func jump_next_madlib():
+	# Do the Jump logic here ourselves so we can use Return in the Dialogic timeline we jumped to.
+	Dialogic.current_state_info['jump_stack'].push_back({'timeline':Dialogic.current_timeline, 'index':Dialogic.current_event_idx, 'label': null})
+	Dialogic.start_timeline(remaining_madlibs.pop_front())
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
